f32fb33ef9d6dc4b157fae75449b2dcc9a8a0328
sheerluck <sheer.luck.andrew@gmail.com>
2021-07-06 22:34:31 +0300

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# fresh-en-passant-checkmates-for-IM-Eric-Rosen
-fresh en passant checkmates for IM Eric Rosen
+
+I have watched **IM Eric Rosen's** [YouTube video](https://www.youtube.com/watch?v=qKX3jVJ5J7E) on en passant checkmates.
+
+I noticed that mark-dev's [ep-mate.md](https://github.com/mark-dev/chessfactory-hall-of-fame/blame/master/etc/docs/results/ep-mate.md) was two years old and https://database.lichess.org shows that amount of games grows every month.
+
+Well, how to get some fresh en passant checkmates without setting Clickhouse and wasting hdd space?
+
+I have written [Python script](en-passant.py) that finds en passant checkmates on the fly without unpacking huge temporary files.
+
+Here are the results of running that script:
+
+[March 2021](March-2021.adoc)
+
+[April 2021](April-2021.adoc)
+
+[May 2021](May-2021.adoc)
+
+[June 2021](June-2021.adoc)
---
+++ en-passant.py
@@ -0,0 +1,206 @@
+"""
+$ bzcat lichess_db_standard_rated_2021-03.pgn.bz2 | python en-passant.py
+$ bzcat lichess_db_standard_rated_2021-04.pgn.bz2 | python en-passant.py
+$ bzcat lichess_db_standard_rated_2021-05.pgn.bz2 | python en-passant.py
+$ bzcat lichess_db_standard_rated_2021-06.pgn.bz2 | python en-passant.py
+"""
+
+import fileinput
+from datetime import datetime
+from re import sub
+
+import parsita as p
+
+
+def now():
+    return f"{datetime.now():%H:%M:%S}"
+
+
+def formatannotations(annotations):
+    return {ant[0]: ant[1] for ant in annotations}
+
+
+def formatgame(game):
+    return {
+        'moves': game[0],
+        'outcome': game[1],
+    }
+
+
+def formatentry(entry):
+    return {'annotations': entry[0], 'game': entry[1]}
+
+
+def handleoptional(optionalmove):
+    if len(optionalmove) > 0:
+        return optionalmove[0]
+    else:
+        return None
+
+
+def grammar() -> p.parsers.RepeatedParser:
+
+    # tokens
+    quote       = p.lit(r'"')
+    whitespace  = p.lit(' ') | p.lit('\n')
+    tag         = p.reg(r'[\u0021-\u0021\u0023-\u005A\u005E-\u007E]+')
+    string      = p.reg(r'[\u0020-\u0021\u0023-\u005A\u005E-\U0010FFFF]+')
+
+    # Annotations: [Foo "Super Awesome Information"]
+    annotation  = '[' >> (tag) << ' ' & (quote >> string << quote) << ']'
+    annotations = p.repsep(annotation, '\n') > formatannotations
+
+    # Moves are more complicated
+    regularmove = p.reg(r'[a-h1-8NBRQKx\+#=]+')
+    longcastle  = p.reg(r'O-O-O[+#]?')           # match first
+    castle      = p.reg(r'O-O[+#]?')
+    nullmove    = p.lit('--')                    # Illegal move
+
+    move        = regularmove | longcastle | castle | nullmove
+
+    # Build up the game
+    movenumber  = (p.reg(r'[0-9]+') << '.' << whitespace) > int
+    turn        = movenumber & (move << whitespace) & (p.opt(move << whitespace) > handleoptional)
+
+    draw        = p.lit('1/2-1/2')
+    white       = p.lit('1-0')
+    black       = p.lit('0-1')
+    outcome     = draw | white | black
+
+    game        = (p.rep(turn) & outcome) > formatgame
+
+    # A PGN entry is annotations and the game
+    entry       = ((annotations << p.rep(whitespace)) & (game << p.rep(whitespace))) > formatentry
+
+    # A file is repeated entries
+    return p.rep(entry)
+
+
+def checkmate(s: str) -> bool:
+    if s[-1] == "#":
+        return True
+    return False
+
+
+def pawn(s: str) -> bool:
+    if s[0] in 'BKNOQR':
+        return False
+    if "=" in s:
+        return False
+    return True
+
+
+def nox(s: str) -> str:
+    t = s.replace("+", '').replace("#", '')
+    q = t.split("x")
+    return q[-1]
+
+
+def withoutx(s: str) -> bool:
+    return "x" not in s
+
+
+def withx(s: str) -> bool:
+    return "x" in s
+
+
+def try_elo(d: dict, line: str) -> dict:
+    for elem in ["WhiteElo", "BlackElo"]:
+        if elem in line:
+            try:
+                d[elem] = int(line.split('"')[1])
+            except Exception as e:
+                pass
+    return d
+
+
+def cleanup(game: str) -> str:
+    no_clock = sub(r"{.+?}",       "",  game)
+    no_dots  = sub(r" \d+\.\.\. ", "",  no_clock)
+    no_space = sub(r"  ",          " ", no_dots)
+    no_marks = sub(r"\?|\!",       "",  no_space)
+    if len(no_marks) > 20:
+        m = no_marks.split(".")
+        no_marks = ".".join([m[0], m[-2], m[-1]])
+    if no_marks.endswith("*\n"):
+        no_marks = no_marks[:-2] + '1/2-1/2\n'
+    return no_marks
+
+
+def process(gr: p.parsers.RepeatedParser,
+            counter: int,
+            num: int,
+            elo: dict, 
+            lines: list[str]) -> tuple[bool, int]:
+    try:
+        parsed = gr.parse("".join(lines)).or_die()
+    except Exception as e:
+        raise ValueError(f"{counter=}") from e
+    for game in parsed:
+        moves = game["game"]["moves"]
+        if len(moves) >= 2:
+            ann = game["annotations"]
+            (n, a, b), (m, c, d) = moves[-2:]
+            moves = [x for x in [a, b, c, d] if x]
+            a, b  = moves[-2:]
+            if all([pawn(a),
+                    pawn(b),
+                    withoutx(a),
+                    withx(b),
+                    checkmate(b),
+                   ]):
+                a, b = nox(a), nox(b)
+                c1, n1 = a
+                c2, n2 = b
+                if c1 == c2:
+                    if [n1, n2] in [['5', '6'], ['4', '3']]:
+                        p = sorted(elo.values())
+                        n = f"| {num:>2} | {counter:>10} "
+                        s = f"| {ann['Site']} | {m:>5} "
+                        e = f"| {p[1]} - {p[0]} |      "
+                        print(f"{n}{s}{e} |", flush=True)
+                        return True, num + 1
+    return False, -1
+
+
+def main() -> int:
+
+    gr = grammar()
+    lines = []
+    step = "annotations"
+    counter = 1
+    num = 1
+    elo = {}
+    print(f"{counter:>10}, {now()}", flush=True)
+    for line in fileinput.input():
+        if len(line) > 1 and step == "annotations":
+            if "Elo" in line:
+                elo = try_elo(elo, line)
+            if "Site" in line:
+                lines.append(line)
+            continue
+        if len(line) == 1 and step == "annotations":
+            lines.append(line)
+            step = "game"
+            continue
+        if len(line) > 1 and step == "game":
+            if min(elo.values()) > 2100:
+                lines.append(cleanup(line))
+            continue
+        if len(line) == 1 and step == "game":
+            if min(elo.values()) > 2100:
+                lines.append(line)
+                is_ok, n = process(gr, counter, num, elo, lines)
+                if is_ok:
+                    num = n
+            lines = []
+            step = "annotations"
+            counter += 1
+            elo = {}
+            if counter % 1_000_000 == 0:
+                print(f"{counter:>10}, {now()}", flush=True)
+    return 0
+
+
+if __name__ == "__main__":
+    exit(main())
